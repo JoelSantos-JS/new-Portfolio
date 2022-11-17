8c2adadfa11b67dcc5105a7a249464583cdbc27c
Joel Santos <joeltere9@gmail.com>
2022-11-17 13:40:39 -0300

    update portfolio

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import pokerdex from '../../assets/projects/pokerdex.gif'
 import rio from '../../assets/projects/rio.png'
 import github from '../../assets/projects/23.gif'
+import portfolio from '../../assets/projects/port.gif'
 function Work() {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[rgb(18,21,32)]'>
@@ -67,6 +68,26 @@ function Work() {
                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#1B1F39]  text-white font-bold text-lg '>Demo</button>
                     </a>
                     <a href="https://github.com/JoelSantos-JS/weather-project" target='_blank'>
+                      <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#1B1F39]  text-white font-bold text-lg '>Code</button>
+                    </a>
+                    
+                  </div>
+              </div>
+            </div>
+            <div style={{backgroundImage:`url(${portfolio})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto container-div'>
+            
+            
+              <div className='opacity-0 group-hover:opacity-100'>
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                Portfolio
+
+                </span>
+
+                  <div className='pt-8 text-center'>
+                    <a href="https://joel-new-portfolio.vercel.app/" target='_blank'>
+                      <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#1B1F39]  text-white font-bold text-lg '>Demo</button>
+                    </a>
+                    <a href="https://github.com/JoelSantos-JS/new-Portfolio" target='_blank'>
                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#1B1F39]  text-white font-bold text-lg '>Code</button>
                     </a>
                     
