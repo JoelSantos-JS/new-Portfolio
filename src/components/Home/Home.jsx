513ea8409332476dee1a31c78658083685bed855
Joel Santos <joeltere9@gmail.com>
2022-11-26 11:24:06 -0300

    update home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <div className='max-w-[1000px] mx-auto px-11 flex flex-col justify-center  h-full' >
                 <p className='text-[#F9004D] text-2xl sm:text-4xl'>Oi , meu nome é </p>
                 <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>Joel Santos</h1>
-                <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>Eu sou Desenvolvedor frontend</h2>
+                <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>Eu sou Desenvolvedor Frontend</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'> {'>'}23 Anos, apaixonado por tecnologia e obcecado em aprender constantemente , procuro uma oportunidade como Desenvolvedor Web.</p>
 
                 <div>
