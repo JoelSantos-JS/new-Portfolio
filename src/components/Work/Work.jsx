260aa917419a777d3dbe58b3d379abe4af5307a2
Joel Santos <joeltere9@gmail.com>
2023-01-08 13:20:08 -0300

    h

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -8,7 +8,7 @@ import portfolio from "../../assets/projects/port.gif";
 import jsmassas from "../../assets/projects/Jsmassas.gif";
 import desafio from "../../assets/projects/17.gif";
 
-import jsextension from "../../assets/projects/js.jpg";
+import jsextension from "../../assets/projects/extension.png";
 function Work() {
   return (
     <div
